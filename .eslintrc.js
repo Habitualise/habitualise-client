c244ea979b479a3b44685c2fc3221bcd730db151
thomasyao15 <64414639+thomasyao15@users.noreply.github.com>
2023-04-19 15:05:10 +1000

    [FEATURE] Add BP react configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: ['@/features/*/*'],
+      },
+    ],
+  },
 };
---
+++ src/language/index.ts
@@ -0,0 +1,3 @@
+export const LABEL = {
+  TEST: 'Test',
+};
