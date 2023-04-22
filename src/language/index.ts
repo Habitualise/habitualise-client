a9fdda8b763aae937ad6f90effb4cd6a6a63453a
thomasyao15 <64414639+thomasyao15@users.noreply.github.com>
2023-04-22 17:33:57 +1000

    feat: complete transfer of all habits screen components
    not including add habit or week view

--- src/language/index.ts
+++ src/language/index.ts
@@ -1,11 +1,5 @@
 export const LABEL = {
-  SIGN_IN_SCREEN: 'Sign In Screen',
-  SIGN_IN: 'Sign In',
-  SIGN_OUT: 'Sign Out',
-  WELCOME_SCREEN: 'Welcome Screen',
-  HOME_SCREEN: 'Home Screen',
   TODAY_SCREEN: 'Today Screen',
-  HABITS_SCREEN: 'Habits Screen',
   LOGGED_IN_AS: (username: string) => `Logged in as ${username}`,
   LOGIN_SCREEN: 'Login Screen',
   WELCOME_HEADER: 'Ready to reach\nyour full potential?',
@@ -14,4 +8,10 @@ export const LABEL = {
   ALREADY_HAVE_ACCOUNT: 'I already have an account',
   TODAY: 'Today',
   TODAY_HEADER: "Today's Habits",
+  HABITS_SCREEN: 'Habits Screen',
+  HABITS: 'Habits',
+  HABITS_HEADER: 'My Habits',
+  ACTIVE: 'Active',
+  ARCHIVED: 'Archived',
+  DAYS_COMPLETED: (percentage: number) => `${percentage}% of days completed`,
 };
