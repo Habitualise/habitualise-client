a2e75be1f128bea13e47c828184e105fc83dd137
thomasyao15 <64414639+thomasyao15@users.noreply.github.com>
2023-04-22 13:20:45 +1000

    [FEATURE] Implement welcome screen (#5)
    * feat: setup fonts, copy over welcome screen
    
    * feat: connected theme colors
    
    * feat: edit header labels
    
    * test: update snapshot

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+import {MD3LightTheme, Provider as PaperProvider} from 'react-native-paper';
 
 import {Router} from './routes';
+import {themeColors} from './theme';
+
+const NavigationTheme = {
+  ...DefaultTheme,
+  dark: true,
+};
+
+const theme = {
+  ...MD3LightTheme,
+  dark: true,
+  colors: themeColors,
+};
 
 export const App = () => {
   return (
-    <NavigationContainer>
-      <Router />
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <NavigationContainer theme={NavigationTheme}>
+        <Router />
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
--- src/features/auth/index.tsx
+++ src/features/auth/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
-
+import {Image, StyleSheet, Pressable} from 'react-native';
+import {Text, Button} from 'react-native-paper';
 import {useAuth0} from 'react-native-auth0';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+import {commonStyles} from '@app/components/styles';
 import {LABEL} from '@app/language';
+import {themeColors} from '@app/theme';
+import PaperView from '@app/components/PaperView';
 
 export const WelcomeScreen = () => {
   const {authorize} = useAuth0();
-  const signIn = async () => {
+  const logIn = async () => {
     try {
       await authorize();
     } catch (e) {
@@ -15,9 +21,73 @@ export const WelcomeScreen = () => {
   };
 
   return (
-    <View>
-      <Text>{LABEL.WELCOME_SCREEN}</Text>
-      <Button title={LABEL.SIGN_IN} onPress={signIn} />
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <PaperView style={commonStyles.container}>
+        <Image
+          source={require('@app/assets/googleLogo.png')}
+          style={styles.logo}
+        />
+        <Text variant="displaySmall" style={styles.header}>
+          {LABEL.WELCOME_HEADER}
+        </Text>
+        <Text variant="titleLarge" style={styles.subHeader}>
+          {LABEL.WELCOME_SUBHEADER}
+        </Text>
+        <Pressable
+          onPress={logIn}
+          style={({pressed}) => [
+            {
+              backgroundColor: pressed
+                ? themeColors.primaryLighter
+                : themeColors.primary,
+            },
+            styles.signUpButtonContainer,
+          ]}
+          android_ripple={{color: themeColors.grey[200]}}>
+          <Text variant="bodyLarge" style={styles.signUpButtonText}>
+            {LABEL.SIGN_UP}
+          </Text>
+        </Pressable>
+        <Button
+          onPress={logIn}
+          contentStyle={{flexDirection: 'row-reverse'}}
+          icon={props => (
+            <MaterialIcons name="arrow-forward" color={props.color} size={20} />
+          )}>
+          {LABEL.ALREADY_HAVE_ACCOUNT}
+        </Button>
+      </PaperView>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  logo: {
+    marginBottom: 50,
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
+  },
+  header: {
+    marginBottom: 15,
+    textAlign: 'center',
+    fontWeight: '700',
+    color: themeColors.primary,
+  },
+  subHeader: {
+    marginBottom: 60,
+    lineHeight: 30,
+    textAlign: 'center',
+    color: themeColors.primary,
+  },
+  signUpButtonContainer: {
+    marginBottom: 10,
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 120,
+  },
+  signUpButtonText: {
+    color: 'white',
+    fontWeight: '500',
+  },
+});
