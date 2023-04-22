7fc8911ec8b4e2ec5ea0e7d4d2aa73e7467cf66e
thomasyao15 <64414639+thomasyao15@users.noreply.github.com>
2023-04-22 17:40:43 +1000

    [FEATURE] Implement habits screen (not including add habit/week view) (#13)
    * feat: complete transfer of all habits screen components
    
    not including add habit or week view
    
    * fix: comment out unused vars

---
+++ src/features/habits/components/Habit.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {Divider, IconButton, Text} from 'react-native-paper';
+
+import {HabitIcon} from '@app/components/HabitIcon';
+import {StyleSheet, View} from 'react-native';
+import {themeColors} from '@app/theme';
+import {LABEL} from '@app/language';
+// import {WeekView} from './WeekView';
+
+interface HabitProps {
+  id: string;
+  name: string;
+  iconName: string;
+  colour: string;
+  completionPercentage: number;
+  status: 'active' | 'archived';
+  completionHistory: string[];
+  daysDue: number[];
+}
+
+export const Habit = (props: HabitProps) => {
+  const {
+    // id,
+    name,
+    iconName,
+    colour,
+    completionPercentage,
+    // status,
+    // completionHistory,
+    // daysDue,
+  } = props;
+
+  return (
+    <>
+      <View style={styles.cardContainer}>
+        <HabitIcon iconName={iconName} colour={colour} />
+        <View style={{flex: 1}}>
+          <Text style={styles.habitName}>{name}</Text>
+          <Text style={styles.habitDaysDue} variant={'bodySmall'}>
+            {LABEL.DAYS_COMPLETED(completionPercentage)}
+          </Text>
+        </View>
+        <IconButton
+          icon={'dots-vertical'}
+          onPress={() => console.log('three dots pressed')}
+        />
+      </View>
+      {/* <WeekView lastSevenCompletions={lastSevenCompletions} daysDue={daysDue} /> */}
+      <Divider />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    padding: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  habitName: {
+    marginBottom: 4,
+    fontWeight: 'bold',
+  },
+  habitDaysDue: {
+    color: themeColors.grey[500],
+  },
+});
--- src/routes/AppTabs.tsx
+++ src/routes/AppTabs.tsx
@@ -4,6 +4,7 @@ import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import {ColorValue} from 'react-native';
 
 import {TodayScreen} from '@app/features/today';
+import {HabitsScreen} from '@app/features/habits';
 import {LABEL} from '@app/language';
 import {useStore, ACTIONS} from '@app/context/StoreContext';
 import {getHabits} from './api/getHabits';
@@ -31,6 +32,20 @@ export const AppTabs = () => {
           tabBarLabel: LABEL.TODAY,
           tabBarIcon: ({color}: {color: ColorValue}) => (
             <MaterialIcons name="event" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={LABEL.HABITS_SCREEN}
+        component={HabitsScreen}
+        options={{
+          tabBarLabel: LABEL.HABITS,
+          tabBarIcon: ({color}) => (
+            <MaterialIcons
+              name="local-fire-department"
+              color={color}
+              size={26}
+            />
           ),
         }}
       />
